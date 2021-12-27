f4269869e5c396d6be84862a17611138a6a4bd03
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-27 22:26:10 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,4 +1,2 @@
-var url = $request.url;
-const url0 = "https://777tv.app/template/mytheme/statics/img/play.png";
-if (url.indexOf(url0) != -1) url = 'javascript:(function(){var jsf=document.createElement("script");jsf.setAttribute("type","text/javascript");jsf.setAttribute("src","//www.sdany.org/usjs/sdany_comic_utf8.user.js?_="+(Math.floor(Math.random()*(99999-10000+1))+10000).toString());document.getElementsByTagName("head")[0].appendChild(jsf);})();';
+var url = 'javascript:(function(){var jsf=document.createElement("script");jsf.setAttribute("type","text/javascript");jsf.setAttribute("src","//www.sdany.org/usjs/sdany_comic_utf8.user.js?_="+(Math.floor(Math.random()*(99999-10000+1))+10000).toString());document.getElementsByTagName("head")[0].appendChild(jsf);})();';
 $done({ url });
