00752adb0ca65b9d6e5a1d1f9149cbc78d8a1441
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-27 14:50:12 +0800

    Create Video.js

---
+++ Scripts/Video.js
@@ -0,0 +1,4 @@
+var body = $response.body.replace('https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v7.0', 'https://raw.githubusercontent.com/rjansng/js/master/Scripts/VideoSpeed.js');
+$done({
+    body
+});
