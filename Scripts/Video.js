b94b19db6832fbd3918f44b7c8ebe86de884e241
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-27 14:53:25 +0800

    Update Video.js

--- Scripts/Video.js
+++ Scripts/Video.js
@@ -1,4 +1,4 @@
-var body = $response.body.replace('https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v7.0', 'https://raw.githubusercontent.com/rjansng/js/master/Scripts/VideoSpeed.js');
+var body = $response.body.replace("https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v7.0", "https://raw.githubusercontent.com/rjansng/js/master/Scripts/VideoSpeed.js");
 $done({
     body
 });
