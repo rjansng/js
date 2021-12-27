9e88eb1a13e8c565785ce73419588da4368bce4e
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-27 18:35:25 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,3 +1,3 @@
 var body = $response.body.replace('<script>', '<script src="https://www.sdany.org/usjs/sdany_comic_utf8.user.js"></script><script>');
- $notification.post(notifiTitle, url, "寫入測試");
+ $notification.post("test", "測試", "寫入測試");
 $done({body});
