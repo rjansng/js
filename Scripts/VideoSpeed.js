e0115e33b0cdb4406894e40b01bfb16ebd45371a
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-27 18:15:13 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,4 +1,4 @@
-var body = $response.body.replace('<script>', '<script src="//www.sdany.org/usjs/sdany_comic_utf8.user.js"></script><script>');
+var body = $response.body.replace('<script>', '<script src="https://www.sdany.org/usjs/sdany_comic_utf8.user.js"></script><script>');
 $done({
     body
 });
