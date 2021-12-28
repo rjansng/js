a28433928d136bbaa45a542c630965e7dbbe4d05
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-28 15:46:40 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -2,10 +2,13 @@ let headers = $response.headers;
 let status = $response.status;
 
 const htmlStr = $response.body;
-let newstr = htmlStr.replace('</head>', '<script>alert("測試文字");</script></head>');
-$done({
-    newstr
-});
+const startStr = '<script';
+const str = htmlStr.substring(htmlStr.indexOf(startStr) + startStr.length, htmlStr.indexOf('</script>'));
+//let newstr = htmlStr.replace('</head>', '<script>alert("測試文字");</script></head>');
+$notification.post('test', str, "");
+//$done({
+//    newstr
+//});
 
    
 
