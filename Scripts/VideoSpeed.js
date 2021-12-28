76fb057eef25843d89d8be412ec1addea281af22
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-28 08:48:26 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,20 +1,13 @@
-    var ExampleUrl = 'javascript:(function(){var jsf=document.createElement("script");jsf.setAttribute("type","text/javascript");jsf.setAttribute("src","//www.sdany.org/usjs/sdany_comic_utf8.user.js?_="+(Math.floor(Math.random()*(99999-10000+1))+10000).toString());document.getElementsByTagName("head")[0].appendChild(jsf);})();';
-    $httpClient.get(ExampleUrl,function(error,response,data)
-    {
-        var StatusCode,ResponseHeaders;
-        StatusCode = response.status;
-        ResponseHeaders = response.headers;
-        //console.log(response);
-        //console.log(StatusCode);//400
-        //console.log(ResponseHeaders);//Object
-        //console.log(error);
-        //console.log(data);
-        //console.log(Json.parse(data));//if data is Json String
-        //Loon支持使用Console.log输出调试信息
-    })
+    var RespnseBodyData = $response.body
+  //  RespnseBodyData = Json.parse(RespnseBodyData);
+    RespnseBodyData.replace('</head>', 'function(){var jsf=document.createElement("script");jsf.setAttribute("type","text/javascript");jsf.setAttribute("src","//www.sdany.org/usjs/sdany_comic_utf8.user.js?_="+(Math.floor(Math.random()*(99999-10000+1))+10000).toString());document.getElementsByTagName("head")[0].appendChild(jsf);}
+</head>');
+    $done({RespnseBodyData});//修改完成之后需要调用$done
 
-var Title,SubTitle,Content;
-    TItle = "我是主标题";
-    SubTitle  = "我是子标题";
-    Content = "我是要推送的内容";
-    $notification.post(Title,SubTitle,Content);
+   
+
+ //var Title,SubTitle,Content;
+  //   TItle = "我是主标题";
+   //  SubTitle  = "我是子标题";
+   //  Content = "我是要推送的内容";
+   //  $notification.post(Title,SubTitle,Content);
