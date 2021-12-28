b90da711dc5dab888df2bd3ad9729d58762bc1a9
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-28 11:15:47 +0800

    Update Video.js

--- Scripts/Video.js
+++ Scripts/Video.js
@@ -1,3 +1,8 @@
 (function() {
-    alert("Not Video");
+    var Title,SubTitle,Content;
+    TItle = "我是主标题";
+    SubTitle  = "我是子标题";
+    Content = "我是要推送的内容";
+    $notification.post(Title,SubTitle,Content);
+}
 })(); 
