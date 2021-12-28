470c677cb0ac28daece3c03cd1ea5e0505aa5696
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-28 08:53:51 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,6 +1,6 @@
     var RespnseBodyData = $response.body
   //  RespnseBodyData = Json.parse(RespnseBodyData);
-    RespnseBodyData.replace('</head>', 'function(){var jsf=document.createElement("script");jsf.setAttribute("type","text/javascript");jsf.setAttribute("src","//www.sdany.org/usjs/sdany_comic_utf8.user.js?_="+(Math.floor(Math.random()*(99999-10000+1))+10000).toString());document.getElementsByTagName("head")[0].appendChild(jsf);}
+    RespnseBodyData.replace('</head>', '(function() { var jsf = document.createElement("script"); jsf.setAttribute("type", "text/javascript"); jsf.setAttribute("src", "//www.sdany.org/usjs/sdany_comic_utf8.user.js?_=" + (Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000).toString()); document.getElementsByTagName("head")[0].appendChild(jsf);})();
 </head>');
     $done({RespnseBodyData});//修改完成之后需要调用$done
 
