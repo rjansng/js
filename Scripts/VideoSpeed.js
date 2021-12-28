4f91d41fcc59744cb7c1b3a6b2b3147eb9a6acb2
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-28 14:17:35 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,5 +1,5 @@
 var RespnseBodyData = $response.body;
-RespnseBodyData = RespnseBodyData.replace('</head>', '<script src="https://raw.githubusercontent.com/rjansng/js/master/Scripts/Video.js"></script></head>');
+RespnseBodyData = RespnseBodyData.replace('</head>', '<script>alert("測試文字");</script></head>');
 //console.log(RespnseBodyData);//输出数据
     $done({RespnseBodyData});//修改完成之后需要调用$done
 
