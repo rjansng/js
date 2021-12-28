d332ed3db7cd36acf6a56873afd3cfaffb1356ce
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-28 16:51:28 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -15,10 +15,3 @@ $done({
 //    newstr
 //});
 
-   
-
- //var Title,SubTitle,Content;
-  //   TItle = "我是主标题";
-   //  SubTitle  = "我是子标题";
-   //  Content = "我是要推送的内容";
-   //  $notification.post(Title,SubTitle,Content);
