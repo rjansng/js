5796ebce0c691ffbb770ede697fce193f8c2397e
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-29 21:41:05 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,3 +1,3 @@
 var body = $response.body;
-body = "<script src='https://raw.githubusercontent.com/rjansng/js/master/Scripts/Video.js'></script>";
+body = body.replace("</head>","<script src='https://raw.githubusercontent.com/rjansng/js/master/Scripts/Video.js'></script></head>";
 $done({body});
