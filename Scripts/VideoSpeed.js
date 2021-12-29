dfa822c3e3307e281eddad905c1f2ddf14383d08
rjansng <66352338+rjansng@users.noreply.github.com>
2021-12-29 13:16:50 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,5 +1,5 @@
 let body = $response.body;
-body = body.replace("<head>", "<head>(function(){var jsf=document.createElement('script');jsf.setAttribute('type','text/javascript');jsf.setAttribute('src','https://raw.githubusercontent.com/rjansng/js/master/Scripts/Video.js?_='+(Math.floor(Math.random()*(99999-10000+1))+10000).toString());document.getElementsByTagName('head')[0].appendChild(jsf);})();</script>");
+body = body.replace("<head>", "<head><script>function(){var jsf=document.createElement('script');jsf.setAttribute('type','text/javascript');jsf.setAttribute('src','https://raw.githubusercontent.com/rjansng/js/master/Scripts/Video.js?_='+(Math.floor(Math.random()*(99999-10000+1))+10000).toString());document.getElementsByTagName('head')[0].appendChild(jsf);};</script>");
   $done({ response: { body } });
 
 //$notification.post('test', "test1", url);
