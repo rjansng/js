6ba933e9e49f357b988ef82320d169eccebaabf9
rjansng <66352338+rjansng@users.noreply.github.com>
2022-01-03 08:16:33 +0800

    Update VideoSpeed.js

--- Scripts/VideoSpeed.js
+++ Scripts/VideoSpeed.js
@@ -1,4 +1,4 @@
-var body = $response.body.replace('</body>', '<script>setTimeout(function(){var script=document.createElement('script');script.src="//raw.githubusercontent.com/rjansng/js/master/Scripts/Video.js";document.getElementsByTagName('head')[0].appendChild(script)},3000);</script></body>');
+var body = $response.body.replace('</body>', '<script>setTimeout(function(){var script=document.createElement('script');script.src="//raw.githubusercontent.com/rjansng/js/master/Scripts/Video.js";document.getElementsByTagName('head')[0].appendChild(script)},6000);</script></body>');
 $done({body});
 //$notification.post("test", "test", body);
 //$done({});
