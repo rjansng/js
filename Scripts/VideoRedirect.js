7bf2e720e75b513a80a71250f899d47b456f1f62
rjansng <66352338+rjansng@users.noreply.github.com>
2022-01-06 15:42:59 +0800

    Update VideoRedirect.js

--- Scripts/VideoRedirect.js
+++ Scripts/VideoRedirect.js
@@ -7,8 +7,8 @@ let notifiTitle = "影片網頁跳轉到影片player";
 
 
 //if (headers.Location.indexOf('777tv') !== -1) {
-body = body.match('/renren.*?"').toString().replace('"','');
-//}
+body = body.match('/renren.*?"').toString();
+//}.replace('"','')
 
 
 $notification.post("test", "test", body);
