4dd05f5856af0b17408ce9251ced3f0ae790ade8
rjansng <66352338+rjansng@users.noreply.github.com>
2022-01-07 19:57:10 +0800

    Create VideoAutoFullScreen.js

---
+++ Scripts/VideoAutoFullScreen.js
@@ -0,0 +1,52 @@
+const body = $response.body;
+const url = $request.url;
+let headers = $response.headers;
+let status = $response.status;
+let str = url.substring(8,11);
+var re2 = new RegExp(/.+\/((777tv|gimy|www.tangrenjie)\.(app|me|tv))\/.+\.html.*$/i);
+
+var startStr = 'var player_data='; //var player_aaaa
+if (/tangrenjie/.test(url)){
+	startStr = 'var player_aaaa=';
+}
+var i1 = body.indexOf(startStr) + startStr.length;
+var i2 = body.indexOf('}', i1);
+
+
+if (re2.test(url) && i1 > 0 && i2 > i1) {
+    try {
+        var str = body.substring(i1, i2 + 1);
+        //console.log('STR:' + str + '\n');
+
+        var json = JSON.parse(str);
+        //console.log('JSON:');
+        //console.log(json);
+        //console.log('\n');
+
+        var url2 = url.replace(re2, '$1');
+        //console.log('URL2:' + url2 + '\n');
+		var str1 = null;
+		var str2 = null;
+		if (/777/.test(url)){
+		    str2 = 'renrenp';
+		}else if (/gimy/.test(url)){
+			str2 = 'jcplayer';
+		}else{
+			
+		}
+		    str1 = 'https://' + url2 + '/' + str2 + '/?url=' + json.url + '&next=//' + url2 + json.link_next;
+   
+        //console.log('STR1:');
+        //console.log(str1);
+        //console.log('\n');
+
+        headers.Location = str1;
+        status = 302;
+        body = '';
+    } catch (e) {
+        console.log('ERROR:');
+        console.log(e);
+        console.log('\n');
+    }
+}
+$done({ status, headers, body });
