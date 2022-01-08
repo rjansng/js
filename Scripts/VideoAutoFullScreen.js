9aac54ce44c8575da62bc4c56a221bd449ce48cb
rjansng <66352338+rjansng@users.noreply.github.com>
2022-01-08 09:54:24 +0800

    Update VideoAutoFullScreen.js

--- Scripts/VideoAutoFullScreen.js
+++ Scripts/VideoAutoFullScreen.js
@@ -2,10 +2,10 @@ const body = $response.body;
 const url = $request.url;
 let headers = $response.headers;
 let status = $response.status;
-var re2 = new RegExp(/.+\/((777tv|gimy|www.tangrenjie)\.(app|me|tv))\/.+\.html.*$/i);
+var re2 = new RegExp(/.+\/((777tv|gimy|www.ysgc)\.(app|me|cc))\/.+\.html.*$/i);
 
 var startStr = 'var player_data='; //var player_aaaa
-if (/tangrenjie/.test(url)){
+if (/ysgc/.test(url)){
 	startStr = 'var player_aaaa=';
 }
 var i1 = body.indexOf(startStr) + startStr.length;
@@ -27,11 +27,16 @@ if (re2.test(url) && i1 > 0 && i2 > i1) {
 		var str1 = null;
 		var str2 = null;
 		if (/777/.test(url)){
-		    str2 = 'renrenp';
+		    	str2 = 'renrenp';
 		}else if (/gimy/.test(url)){
 			str2 = 'jcplayer';
+		}else if (/ysgc/.test(url)){
+			str2 = 'ddplay';
+			url2 = 'bo.movie06.com';
+			//https://bo.movie06.com/ddplay/?url=https://fendou.duoduozy.com/m3u8/202112/9/ab83797c2048f6b1875d1e290d5bc392c5a7fb49.m3u8&jump=/vodplay/48098-1-2.html
 		}else{
-			
+			body = '';
+			$done({});
 		}
 		    str1 = 'https://' + url2 + '/' + str2 + '/?url=' + json.url + '&next=//' + url2 + json.link_next;
    
