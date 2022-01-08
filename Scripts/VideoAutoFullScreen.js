dd3925488af1ea85ef2c6873087efe885f98358b
rjansng <66352338+rjansng@users.noreply.github.com>
2022-01-08 10:20:13 +0800

    Update VideoAutoFullScreen.js

--- Scripts/VideoAutoFullScreen.js
+++ Scripts/VideoAutoFullScreen.js
@@ -33,12 +33,13 @@ if (re2.test(url) && i1 > 0 && i2 > i1) {
 		}else if (/ysgc/.test(url)){
 			str2 = 'ddplay';
 			url2 = 'bo.movie06.com';
+			json.url =unescape(base64decode(json.url));
 			//https://bo.movie06.com/ddplay/?url=https://fendou.duoduozy.com/m3u8/202112/9/ab83797c2048f6b1875d1e290d5bc392c5a7fb49.m3u8&jump=/vodplay/48098-1-2.html
 		}else{
 			body = '';
 			$done({});
 		}
-		    str1 = 'https://' + url2 + '/' + str2 + '/?url=' + json.url + '&next=//' + url2 + json.link_next;
+		    str1 = 'https://' + url2 + '/' + str2 + '/?url=' + json.url + '&next=//' + json.link_next;
    
         //console.log('STR1:');
         //console.log(str1);
