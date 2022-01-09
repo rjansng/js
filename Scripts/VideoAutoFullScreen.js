e8ebe955b60edf74f3020bb6f50b739763084ad1
rjansng <66352338+rjansng@users.noreply.github.com>
2022-01-09 14:27:27 +0800

    Update VideoAutoFullScreen.js

--- Scripts/VideoAutoFullScreen.js
+++ Scripts/VideoAutoFullScreen.js
@@ -2,57 +2,262 @@ const body = $response.body;
 const url = $request.url;
 let headers = $response.headers;
 let status = $response.status;
+status = 302;
 var re2 = new RegExp(/.+\/((777tv|gimy|www.ysgc)\.(app|me|cc))\/.+\.html.*$/i);
 
 var startStr = 'var player_data='; //var player_aaaa
-if (/ysgc/.test(url)){
-	startStr = 'var player_aaaa=';
+if (/ysgc/.test(url)) {
+    startStr = 'var player_aaaa=';
+    console.log(startStr);
 }
 var i1 = body.indexOf(startStr) + startStr.length;
 var i2 = body.indexOf('}', i1);
+var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+var base64DecodeChars = new Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
 
+function base64encode(str) {
+    var out, i, len;
+    var c1, c2, c3;
+    len = str.length;
+    i = 0;
+    out = "";
+    while (i < len) {
+        c1 = str.charCodeAt(i++) & 0xff;
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break
+        }
+        c2 = str.charCodeAt(i++);
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+            out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break
+        }
+        c3 = str.charCodeAt(i++);
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+        out += base64EncodeChars.charAt(c3 & 0x3F)
+    }
+    return out
+}
+
+function base64decode(str) {
+    var c1, c2, c3, c4;
+    var i, len, out;
+    len = str.length;
+    i = 0;
+    out = "";
+    while (i < len) {
+        do {
+            c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff]
+        } while (i < len && c1 == -1);
+        if (c1 == -1) break;
+        do {
+            c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff]
+        } while (i < len && c2 == -1);
+        if (c2 == -1) break;
+        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+        do {
+            c3 = str.charCodeAt(i++) & 0xff;
+            if (c3 == 61) return out;
+            c3 = base64DecodeChars[c3]
+        } while (i < len && c3 == -1);
+        if (c3 == -1) break;
+        out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+        do {
+            c4 = str.charCodeAt(i++) & 0xff;
+            if (c4 == 61) return out;
+            c4 = base64DecodeChars[c4]
+        } while (i < len && c4 == -1);
+        if (c4 == -1) break;
+        out += String.fromCharCode(((c3 & 0x03) << 6) | c4)
+    }
+    return out
+}
+
+function utf16to8(str) {
+    var out, i, len, c;
+    out = "";
+    len = str.length;
+    for (i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if ((c >= 0x0001) && (c <= 0x007F)) {
+            out += str.charAt(i)
+        } else if (c > 0x07FF) {
+            out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+            out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F))
+        } else {
+            out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F))
+        }
+    }
+    return out
+}
+
+function utf8to16(str) {
+    var out, i, len, c;
+    var char2, char3;
+    out = "";
+    len = str.length;
+    i = 0;
+    while (i < len) {
+        c = str.charCodeAt(i++);
+        switch (c >> 4) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+            out += str.charAt(i - 1);
+            break;
+        case 12:
+        case 13:
+            char2 = str.charCodeAt(i++);
+            out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+            break;
+        case 14:
+            char2 = str.charCodeAt(i++);
+            char3 = str.charCodeAt(i++);
+            out += String.fromCharCode(((c & 0x0F) << 12) | ((char2 & 0x3F) << 6) | ((char3 & 0x3F) << 0));
+            break
+        }
+    }
+    return out
+}
+
+const $tool = tool();
+
+function tool() {
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const isResponse = typeof $response != "undefined"
+    const node = (() => {
+        if (typeof require == "function") {
+            const request = require('request')
+            return ({
+                request
+            })
+        } else {
+            return (null)
+        }
+    })()
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message)
+        if (isSurge) $notification.post(title, subtitle, message)
+        if (node) console.log(JSON.stringify({
+            title, subtitle, message
+        }));
+    }
+    const write = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key)
+        if (isSurge) return $persistentStore.write(value, key)
+    }
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key)
+        if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = {
+                url: options
+            }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.get(options, (error, response, body) => {
+            callback(error, adapterStatus(response), body)
+        })
+        if (node) {
+            node.request(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = {
+                url: options
+            }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (node) {
+            node.request.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    return {
+        isQuanX, isSurge, isResponse, notify, write, read, get, post
+    }
+}
 
 if (re2.test(url) && i1 > 0 && i2 > i1) {
     try {
-        var str = body.substring(i1, i2 + 1);
-        //console.log('STR:' + str + '\n');
-
-        var json = JSON.parse(str);
-        //console.log('JSON:');
-        //console.log(json);
-        //console.log('\n');
-
+        var strr = body.substring(i1, i2 + 1);
+        var json = JSON.parse(strr);
         var url2 = url.replace(re2, '$1');
         //console.log('URL2:' + url2 + '\n');
-		var str1 = null;
-		var str2 = null;
-		if (/777/.test(url)){
-		    	str2 = 'renrenp';
-		}else if (/gimy/.test(url)){
-			str2 = 'jcplayer';
-		}else if (/ysgc/.test(url)){
-			str2 = 'ddplay';
-			url2 = 'bo.movie06.com';
-			json.url = unescape(base64decode(json.url));
-			console.log(json.url);
-			//https://bo.movie06.com/ddplay/?url=https://fendou.duoduozy.com/m3u8/202112/9/ab83797c2048f6b1875d1e290d5bc392c5a7fb49.m3u8&jump=/vodplay/48098-1-2.html
-		}else{
-			body = '';
-			$done({});
-		}
-		    str1 = 'https://' + url2 + '/' + str2 + '/?url=' + json.url + '&next=//' + json.link_next;
-   
-        //console.log('STR1:');
-        //console.log(str1);
-        //console.log('\n');
-
+        var str1 = null;
+        var str2 = null;
+        console.log(url2);
+        if (/777/.test(url)) {
+            str2 = 'renrenp';
+        } else if (/gimy/.test(url)) {
+            str2 = 'jcplayer';
+        } else if (/ysgc/.test(url)) {
+            json.url = unescape(base64decode(json.url));
+            console.log(json.url);
+			headers.Location = "https://jiexi.ysgc.xyz/?url=" + json.url;
+            //if (json.from == "duoduozy") {
+            //    headers.Location = "https://jiexi.ysgc.xyz/?url=" + json.url;
+           // } else {
+            //    headers.Location = "https://jiexi.ysgc.xyz/?url=" + json.url;
+           // }
+            $done({
+                status, headers, body
+            });
+        } else {
+            body = '';
+            $done({});
+        }
+        str1 = 'https://' + url2 + '/' + str2 + '/?url=' + json.url + '&next=//' + json.link_next;
         headers.Location = str1;
-        status = 302;
         body = '';
     } catch (e) {
-        console.log('ERROR:');
-        console.log(e);
-        console.log('\n');
+        //console.log('ERROR:');
+        //console.log(e);
+        //console.log('\n');
     }
 }
-$done({ status, headers, body });
+$done({
+    status, headers, body
+});
