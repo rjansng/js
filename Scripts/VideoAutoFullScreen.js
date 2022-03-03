6f7bb5551d9553274c1c8749ec4f43e35fd09101
rjansng <66352338+rjansng@users.noreply.github.com>
2022-03-03 21:57:33 +0800

    Update VideoAutoFullScreen.js

--- Scripts/VideoAutoFullScreen.js
+++ Scripts/VideoAutoFullScreen.js
@@ -7,7 +7,7 @@ status = 302;
 var re2 = new RegExp(/.+\/((777tv|gimy|www.ysgc|gimytv|www.tkys)\.(app|me|cc|com|tv))\/.+\.html.*$/i);
 
 var startStr = 'var player_data='; //var player_aaaa
-if ((/ysgc/.test(url)) || (/tkys/.test(url)))  {
+if ((/ysgc/.test(url)) || (/tkys/.test(url))) {
     startStr = 'var player_aaaa=';
     console.log(startStr);
 }
@@ -15,18 +15,19 @@ var i1 = body.indexOf(startStr) + startStr.length;
 var i2 = body.indexOf('}', i1);
 
 function ysgc(search_url) {
-   const options = {
+    const options = {
         url: search_url,
         headers: {
             "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
             "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 - mmbWebBrowse - ios",
-       Referer: "https://www.ysgc.cc/"
+            Referer: "https://www.ysgc.cc/"
         }
     }
-console.log(search_url);
-	$.get(options, (err, resp, data) => {$persistentStore.write(data, 'ysgc');
-	})
-	 $.done()
+    console.log(search_url);
+    $.get(options, (err, resp, data) => {
+        $persistentStore.write(data, 'ysgc');
+    })
+    $.done()
 }
 
 if (re2.test(url) && i1 > 0 && i2 > i1) {
@@ -41,24 +42,38 @@ if (re2.test(url) && i1 > 0 && i2 > i1) {
         if (/777/.test(url)) {
             str1 = 'https://' + url2 + '/renrenp/?url=' + json.url + '&next=//777tv.app' + json.link_next;
         } else if (/gimy/.test(url)) {
-	    str1 = 'https://' + url2 + '/jcplayer/?url=' + json.url + '&next=//gimy.app' + json.link_next;
+            str1 = 'https://' + url2 + '/jcplayer/?url=' + json.url + '&next=//gimy.app' + json.link_next;
         } else if (/ysgc/.test(url)) {
-            str1 = 'https://jiexi.ysgc.xyz/duoduo/?url=' +  json.url + '&jump=//' + json.link_next;
+            str1 = 'https://jiexi.ysgc.xyz/duoduo/?url=' + json.url + '&jump=//' + json.link_next;
         } else if (/tkys/.test(url)) {
-            str1 = 'https://play.tkys.tv/laodi.php/?url=' +  json.url + '&jump=//' + json.link_next;
+	    var str3 = null;
+            switch (json.from) {
+            case "ftqp4":case "laodim3u8":case "laodiyun":
+                str3 = 'https://play.tkys.tv/laodi.php/?url=';
+                break;
+            case "xg_app_player":
+                str3 = 'https:\/\/www.x-n.cc\/api.php?url=';
+                break;
+            default:
+                 str3 = 'https://jxqd.tkys.tv/dnmd.php?url=';
+            }
+
+            str1 = str3 + json.url + '&jump=//' + json.link_next;
         } else {
             $done({});
         }
         str1 = '<meta http-equiv="refresh" content="0;url=' + str1 + '" />';
-       // headers.Location = str1;
-	$done({body:str1});
+        // headers.Location = str1;
+        $done({
+            body: str1
+        });
     } catch (e) {
         //console.log('ERROR:');
         //console.log(e);
         //console.log('\n');
-	$done({});
+        $done({});
     }
- 
+
 
 }
 
@@ -68,35 +83,58 @@ if (re2.test(url) && i1 > 0 && i2 > i1) {
 
 
 function Env() {
-  LN = typeof $loon != "undefined"
-  SG = typeof $httpClient != "undefined" && !LN
-  QX = typeof $task != "undefined"
-  read = (key) => {
-    if (LN || SG) return $persistentStore.read(key)
-    if (QX) return $prefs.valueForKey(key)
-  }
-  write = (key, val) => {
-    if (LN || SG) return $persistentStore.write(key, val); 
-    if (QX) return $prefs.setValueForKey(key, val)
-  }
-  notice = (title, subtitle, message, url) => {
-    if (LN) $notification.post(title, subtitle, message, url)
-    if (SG) $notification.post(title, subtitle, message, { url: url })
-    if (QX) $notify(title, subtitle, message, { "open-url": url })
-  }
-  get = (url, cb) => {
-    if (LN || SG) {$httpClient.get(url, cb)}
-    if (QX) {url.method = 'GET'; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
-  }
-  post = (url, cb) => {
-    if (LN || SG) {$httpClient.post(url, cb)}
-    if (QX) {url.method = 'POST'; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
-  }
-  put = (url, cb) => {
-    if (LN || SG) {$httpClient.put(url, cb)}
-    if (QX) {url.method = 'PUT'; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
-  }
-  log = (message) => console.log(message)
-  done = (value = {}) => {$done(value)}
-  return { LN, SG, QX, read, write, notice, get, post, put, log, done }
+    LN = typeof $loon != "undefined"
+    SG = typeof $httpClient != "undefined" && !LN
+    QX = typeof $task != "undefined"
+    read = (key) => {
+        if (LN || SG) return $persistentStore.read(key)
+        if (QX) return $prefs.valueForKey(key)
+    }
+    write = (key, val) => {
+        if (LN || SG) return $persistentStore.write(key, val);
+        if (QX) return $prefs.setValueForKey(key, val)
+    }
+    notice = (title, subtitle, message, url) => {
+        if (LN) $notification.post(title, subtitle, message, url)
+        if (SG) $notification.post(title, subtitle, message, {
+            url: url
+        })
+        if (QX) $notify(title, subtitle, message, {
+            "open-url": url
+        })
+    }
+    get = (url, cb) => {
+        if (LN || SG) {
+            $httpClient.get(url, cb)
+        }
+        if (QX) {
+            url.method = 'GET';
+            $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+        }
+    }
+    post = (url, cb) => {
+        if (LN || SG) {
+            $httpClient.post(url, cb)
+        }
+        if (QX) {
+            url.method = 'POST';
+            $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+        }
+    }
+    put = (url, cb) => {
+        if (LN || SG) {
+            $httpClient.put(url, cb)
+        }
+        if (QX) {
+            url.method = 'PUT';
+            $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+        }
+    }
+    log = (message) => console.log(message)
+    done = (value = {}) => {
+        $done(value)
+    }
+    return {
+        LN, SG, QX, read, write, notice, get, post, put, log, done
+    }
 }
