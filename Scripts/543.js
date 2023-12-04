9a57096d129da5c1a0ce3508328b2cab21c529a2
rjansng <66352338+rjansng@users.noreply.github.com>
2023-12-04 16:40:12 +0800

    Update 543.js

--- Scripts/543.js
+++ Scripts/543.js
@@ -1,2 +1,2 @@
-let body = $response.body.replace("</body>","<script>(()=>{setTimeout(function(){document.querySelector('div[style] a button').parentElement.parentElement.remove();var xx =document.querySelectorAll('.gadBlock');xx[0].innerHTML="";xx[1].innerHTML="";xx[2].innerHTML="";},500);})();</script></body>");
+let body = $response.body.replace("</body>","<script>(()=>{setTimeout(function(){document.querySelector('div[style] a button').parentElement.parentElement.remove();var xx =document.querySelectorAll('.gadBlock');xx[0].innerHTML="";},500);})();</script></body>");
 $done({body});
