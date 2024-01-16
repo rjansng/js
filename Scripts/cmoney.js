b86738c380b7ea140eea092a355eedcd45b67168
rjansng <66352338+rjansng@users.noreply.github.com>
2024-01-16 08:43:43 +0800

    1

---
+++ Scripts/cmoney.js
@@ -0,0 +1,7 @@
+let inapp_data = {
+  "unactivatedAuthDays": 365,
+  "serverTime": 1705045278699,
+  "expiredTime": 1736987566344
+}
+
+$done({ response: { status: 200, body: JSON.stringify(inapp_data) } });
