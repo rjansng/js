9335267b321d67e6bf03e5ac1b488ee80d34955d
rjansng <66352338+rjansng@users.noreply.github.com>
2024-05-13 10:38:41 +0800

    Update netflix.js

--- Scripts/netflix/netflix.js
+++ Scripts/netflix/netflix.js
@@ -1,3 +1,5 @@
+delete $request.headers["X-Xss-Protection"];
+delete $request.headers["x-xss-protection"];
 delete $request.headers["X-Frame-Options"];
 delete $request.headers["x-frame-options"];
 $done({headers:$request.headers});
