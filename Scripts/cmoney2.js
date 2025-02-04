b8ee33a72f3eb5c256f3d3e82d06191a2b27f5a4
rjansng <66352338+rjansng@users.noreply.github.com>
2025-02-04 15:52:32 +0800

    Update cmoney2.js

--- Scripts/cmoney2.js
+++ Scripts/cmoney2.js
@@ -1,5 +1,5 @@
 (async () => {
   let et = Date.now() + (365 * 24 * 60 * 60 * 1000);
-  let rbody = { "expiredTime": et, "serverTime": Date.now(), "unactivatedAuthDays": et };
+  let rbody = { "expiredTime": et, "serverTime": Date.now(), "unactivatedAuthDays": 90 };
   $done({ response: { status: 200, headers: {}, body: JSON.stringify(rbody) } });
 })();
